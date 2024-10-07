c25e646177303839cf91c65cbb1d6af2f154abf0
FergusonLyn <psky730@gmail.com>
2024-10-07 01:41:44 +0100

    set up sidebars on mobile and pc screens

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,35 @@
 
+
+
+// import SideBar
+//  from "@/components/SideBar";
+
+import MobileNav from "@/components/MobileNav";
+import SideBar from "@/components/SideBar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: "Ferguson" , lastName: "Tetteh" }
   return (
-    <main>
-        SIDEBAR 
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <SideBar user={loggedIn} />
+       
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+            <div>
+                <MobileNav user={loggedIn} />
+            </div>
+          </div>
+          {children}
+        </div>
+
     </main>
   );
 }
---
+++ components/RightSidebar.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+
+const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
+  return (
+    <aside className="right-sidebar">
+        <section className="flex flex-col pb-8">
+            <div className="profile-banner" />
+
+        </section>
+
+      
+    </aside>
+  )
+}
+
+export default RightSidebar
